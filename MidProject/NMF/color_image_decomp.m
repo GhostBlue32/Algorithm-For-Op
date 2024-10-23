7a5305388409bcc8768262ee6b06215f5cd1584d
GhostBlue32 <haoen.huang@icloud.com>
2024-10-22 21:19:20 -0400

    final alter

--- MidProject/NMF/color_image_decomp.m
+++ MidProject/NMF/color_image_decomp.m
@@ -1,21 +1,20 @@
 function color_image_decomp()
+    C = imread('Pro.jpg');  % This image is in uint8 format.
+    % Convert to double before processing.
+    Cr = double(C);
+    [r,c,fo] = size(Cr);
+    fprintf('Image size = [%d,%d,%d].\n', r, c,fo)
     tic;
-    for p = 100:50:300
+    startp = floor(r / 6);
+    endp = floor(r / 2);
+    for p = startp:50:endp
         
         starttime = toc;
         tol = 1e-3;
-    
-        C = imread('Pro.jpg');  % This image is in uint8 format.
-        
+            
         % Plot image.  Note that I have made the image small so I don't
         % need to deal with gigantic matrices.  Therefore, I need to
         % magnify the image to display it successfully.
-        
-        
-        % Convert to double before processing.
-        Cr = double(C);
-        [r,c,fo] = size(Cr);
-        fprintf('Image size = [%d,%d,%d].\n', r, c,fo)
         
         % Initialize factor matrices
         W0 = 5*rand(r,p);
@@ -33,11 +32,11 @@ function color_image_decomp()
         [W1,H1] = nonnegative_matrix_factorization(Cr(:,:,2), W1, H1, tol);
         [W2,H2] = nonnegative_matrix_factorization(Cr(:,:,3), W2, H2, tol);
         
-        [r,c,fo] = size(W0);  
-        fprintf('W size = [%d,%d].\n', r, c)  
+        [r1,c1,fo] = size(W0);  
+        fprintf('W size = [%d,%d].\n', r1, c1)  
         
-        [r,c,fo] = size(H0);  
-        fprintf('H size = [%d,%d].\n', r, c)  
+        [r2,c2,fo] = size(H0);  
+        fprintf('H size = [%d,%d].\n', r2, c2)  
         
         % Reconstruct matrix and display it.
         R = uint8(W0 * H0);  % Red channel
